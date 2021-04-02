94e46fc69d066866dbc7b4968536b0f5e3a79271
Caroline Tan <carolinectan3@gmail.com>
2021-04-02 17:55:05 -0400

    Add developer hash

---
+++ models/model_b.rb
@@ -0,0 +1,11 @@
+developer = {
+  "name" => "Parker",
+  "school" => "Turing",
+  "age" => 31,
+  "weight" => 142.0,
+  "is_human" => true,
+  "is_smart" => true,
+  "interests" => ["cycling", "dogs", "hiking", "climbing"],
+  "favorite_foods" => ["bean burritos", "pizza", "tacos"]
+
+}
