b6c944a864d2d7518ec007b5a815d22ab4607c53
Caroline Tan <carolinectan3@gmail.com>
2021-04-02 17:48:23 -0400

    Add dinosaur hash

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+dinosaur = {
+  "color" => "green",
+  "type" => "Stegosaurus",
+  "height_in_feet" => 9.0,
+  "weight_in_lbs" => 6800,
+  "is_carnivore" => false,
+  "is_herbivore" => true,
+  "location" => ["Colorado", "Montana", "Utah"],
+  "physical_characteristic" => ["4 legs", "bony plates on back", "spiked tail"]
+
+}
